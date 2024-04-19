ccd7748cc9d666d1df7c1c0a69f757eec0372a37
CSeungJoo <seungjoo13579@gmail.com>
2024-04-19 13:40:25 +0900

    init

---
+++ src/main/java/kr/pah/pcs/yestv/YesTvApplication.java
@@ -0,0 +1,13 @@
+package kr.pah.pcs.yestv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesTvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YesTvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/pah/pcs/yestv/YesTvApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pah.pcs.yestv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesTvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
