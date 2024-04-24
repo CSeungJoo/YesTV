2cd5532b4419889faffef4ea325041f1282e8c82
CSeungJoo <seungjoo13579@gmail.com>
2024-04-24 21:03:50 +0900

    타임리프용 컨트롤러 생성

---
+++ src/main/java/kr/pah/pcs/yestv/web/controller/webController.java
@@ -0,0 +1,21 @@
+package kr.pah.pcs.yestv.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class webController {
+
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+}
