5f179100ad45b96dbbb9c9ad4d4af48e3857b7dd
CSeungJoo <seungjoo13579@gmail.com>
2024-05-01 18:58:55 +0900

    유저 회원가입시 기존 유저와 username이 동일하면 에러를 throw하는 코드 추가

--- src/main/java/kr/pah/pcs/yestv/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/kr/pah/pcs/yestv/domain/user/service/impl/UserServiceImpl.java
@@ -1,32 +1,44 @@
 package kr.pah.pcs.yestv.domain.user.service.impl;
 
-import kr.pah.pcs.yestv.domain.location.domain.Location;
+import kr.pah.pcs.yestv.domain.location.service.LocationService;
 import kr.pah.pcs.yestv.domain.user.dto.CreateUserDto;
-import kr.pah.pcs.yestv.domain.user.entity.User;
+import kr.pah.pcs.yestv.domain.user.domain.User;
 import kr.pah.pcs.yestv.domain.user.repository.UserRepository;
 import kr.pah.pcs.yestv.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder pwdEncoder;
+    private final LocationService locationService;
+
 
     @Override
+    @Transactional
     public User createUser(CreateUserDto createUserDto) {
+        Optional<User> userOpt = userRepository.getUserByUsername(createUserDto.getUsername());
 
-        User user = User.builder()
-                .username(createUserDto.getUsername())
-                .nickname(createUserDto.getNickname())
-                .password(pwdEncoder.encode(createUserDto.getPassword()))
-                .email(createUserDto.getEmail())
-                .build();
+        if(userOpt.isPresent())
+            throw new IllegalStateException("이미 존재하는 유저입니다.");
 
-        return userRepository.save(user);
+        User createUser = User.builder()
+                    .username(createUserDto.getUsername())
+                    .nickname(createUserDto.getNickname())
+                    .password(pwdEncoder.encode(createUserDto.getPassword()))
+                    .email(createUserDto.getEmail())
+                    .location(locationService.getLocationByIdx(createUserDto.getLocation_id()))
+                    .build();
+
+            return userRepository.save(createUser);
     }
 
     @Override
@@ -37,16 +49,19 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public User updateUser(User user) {
         return userRepository.save(user);
     }
 
     @Override
+    @Transactional
     public void deleteUser(int idx) {
         getUserByIdx(idx).changeDelete();
     }
 
     @Override
+    @Transactional
     public void deleteUser(User user) {
         user.changeDelete();
     }
